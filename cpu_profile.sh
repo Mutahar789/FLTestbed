c08642765e914db36045c0efa223b1f1d9571e36
mutahar789 <mutahar789@gmail.com>
2023-01-26 09:48:50 +0500

    typo

--- cpu_profile.sh
+++ cpu_profile.sh
@@ -2,3 +2,14 @@
 
 cd pygrid-federated-feature-federated_process/examples/model-centric/data/
 
+# Nexus 6P
+adb -s 84B5T15B03015109 shell top | grep openmined > "Nexus 69_cpu_info"
+
+# Nexus 5x
+adb -s 00f2aa907991b4e1 shell top | grep openmined > "Nexus 5x_cpu_info"
+
+# Nexus 5
+adb -s 0ba3223902e1a4b4 shell top | grep openmined > "Nexus 5_cpu_info"
+
+# Nokia 1
+adb -s FRTBA80314746200 shell top | grep openmined > "Nokia 1_cpu_info"
