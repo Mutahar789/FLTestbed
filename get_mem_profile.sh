75a5056d6f552825d3e4a83164cac83e677c47a1
mutahar789 <mutahar789@gmail.com>
2023-01-26 09:38:18 +0500

    get_mem_profile

---
+++ get_mem_profile.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cd pygrid-federated-feature-federated_process/examples/model-centric/data/
+
+# Nexus 6P
+adb -s 84B5T15B03015109 shell "su -c cat /data/user/0/org.openmined.syft.demo/files/mydir/memoryinfo.csv" > "Nexus 6P_memLog.csv"
+# Nexus 5x
+adb -s 00f2aa907991b4e1 shell "su -c cat /data/user/0/org.openmined.syft.demo/files/mydir/memoryinfo.csv" > "Nexus 5x_memLog.csv"
+# Nexus 5
+adb -s 0ba3223902e1a4b4 shell "cat /data/user/0/org.openmined.syft.demo/files/mydir/memoryinfo.csv" > "Nexus 5_memLog.csv"
+# Nokia 1
+adb -s FRTBA80314746200 shell "su -c cat /data/user/0/org.openmined.syft.demo/files/mydir/memoryinfo.csv" > "Nokia 1_memLog.csv"
