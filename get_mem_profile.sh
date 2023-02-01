c21200d90a7eac6eb9309d9b6fb40b79bed983f5
mutahar789 <mutahar789@gmail.com>
2023-02-01 16:18:27 +0500

    Updated Nexus 6P device id

--- get_mem_profile.sh
+++ get_mem_profile.sh
@@ -3,7 +3,7 @@
 cd pygrid-federated-feature-federated_process/examples/model-centric/data/
 
 # Nexus 6P
-adb -s 84B5T15B03015109 shell "su -c cat /data/user/0/org.openmined.syft.demo/files/mydir/memoryinfo.csv" > "Nexus 6P_memLog.csv"
+adb -s 84B7N15A07007998 shell "su -c cat /data/user/0/org.openmined.syft.demo/files/mydir/memoryinfo.csv" > "Nexus 6P_memLog.csv"
 
 # Nexus 5x
 adb -s 00f2aa907991b4e1 shell "su -c cat /data/user/0/org.openmined.syft.demo/files/mydir/memoryinfo.csv" > "Nexus 5x_memLog.csv"
