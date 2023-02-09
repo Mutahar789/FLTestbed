0b8fdb4869104375e4bfb97e634a87dd9b82dda5
mutahar789 <mutahar789@gmail.com>
2023-02-09 16:31:57 +0500

    CPU profiling plotting

--- get_cpu_profile.sh
+++ get_cpu_profile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-  adb -s $1 shell cat /proc/stat >> cpu_profile
-  echo "###" >> cpu_profile
+  adb -s $1 shell cat /proc/stat >> ./pygrid-federated-feature-federated_process/examples/model-centric/data/$2_cpu_profile.txt
+  echo "###" >> ./pygrid-federated-feature-federated_process/examples/model-centric/data/$2_cpu_profile.txt
   sleep 1
 done
 
